c0aa5899f52555f6b5a36ea70ab4a00168c610e6
Marina Tukalo <marina-tukalo@hotmail.com>
2019-10-19 20:56:18 +0200

    modified version

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ var Location = /** @class */ (function () {
         this.address = address;
     }
     Location.prototype.display = function () {
-        return "\n\t\t\t\t<img class=\"img-fluid card-img-top d-none d-sm-block\" src=\"img/" + this.img + "\">\n\t\t\t\t" + this.place + "<br><hr>\n\t\t\t\t" + this.zip + "<br><hr>  \n\t\t\t\t" + this.address + " \n\t\t\t";
+        return "\n\t\t\t\t<img class=\"img-fluid card-img-top d-sm-none d-md-block\" src=\"img/" + this.img + "\">\n\t\t\t\t" + this.place + "<br><hr>\n\t\t\t\t" + this.zip + "<br><hr>  \n\t\t\t\t" + this.address + " \n\t\t\t";
     };
     return Location;
 }());
@@ -53,14 +53,14 @@ var Event = /** @class */ (function (_super) {
 }(Location));
 var Church_V = new Location("vienna_1.jpg", "ST. CHARLES CHURCH", "1010 Vienna", "Karlsplatz 1");
 var Zoo_V = new Location("vienna_2.jpg", "ZOO VIENNA", "1130 Vienna", "Maxingstraße 13b");
-var Thai_V = new Food("vienna_3.jpg", "LEMON LEAF THAI RESTAURANT", "1050 Vienna", "Kettenbrückengasse 19", 5812308, "Thai", "www.lemonleaf.at");
-var Sixta_V = new Food("vienna_4.jpg", "SIXTA RESTAURANT", "1050 Vienna", "Schönbrunner Straße 21", 5852856, "traditional austrian", "www.sixta-restaurant.at");
+var Thai_V = new Food("vienna_3.jpg", "LEMON LEAF THAI RESTAURANT", "1050 Vienna", "Kettenbrückengasse 19", 5812308, "Thai", "http://www.lemonleaf.at/");
+var Sixta_V = new Food("vienna_4.jpg", "SIXTA RESTAURANT", "1050 Vienna", "Schönbrunner Straße 21", 5852856, "traditional austrian", "http://www.sixta-restaurant.at/");
 var Music_V = new Event("vienna_5.jpg", "MUSIKVEREIN: MOSKAUER VIRTUOSEN", "1010 Vienna", "Musikvereinsplatz 1", "25.10.2019", "19.30", "45 EUR");
 var Gallery_V = new Event("vienna_6.jpg", "ALBERTINA: MARIA LASSNIG", "1010 Vienna", "Albertinaplatz 1", "29.10.2019", "16.00", "35 EUR");
 var Opera_S = new Location("sydney_1.jpg", "SYDNEY OPERA HOUSE", "2000 Sydney", "Bennelong Point");
 var Pool_S = new Location("sydney_2.jpg", "BONDI ICEBERGS POOL Sydney", "2026 Sydney", "1 Notts Ave,Bondi Beach");
-var Greek_S = new Food("sydney_3.jpg", "The RESTAURANT ALPHA", "2000 Sydney", "238 Castlereagh St", 90981111, "greek", "www.alpharestaurant.com.au");
-var Italian_S = new Food("sydney_4.jpg", "The RESTAURANT PENDOLINO", "2000 Sydney", "100/412-414 George Street LEVEL 2 THE STRAND ARCADE", 92316117, "italian", "www.pendolino.com.au");
+var Greek_S = new Food("sydney_3.jpg", "The RESTAURANT ALPHA", "2000 Sydney", "238 Castlereagh St", 90981111, "greek", "http://www.alpharestaurant.com.au/");
+var Italian_S = new Food("sydney_4.jpg", "The RESTAURANT PENDOLINO", "2000 Sydney", "100/412-414 George Street LEVEL 2 THE STRAND ARCADE", 92316117, "italian", "https://www.pendolino.com.au/");
 var Cirque_S = new Event("sydney_5.jpg", "CIRQUE DU SOLEIL KURIOS", "2000 Sydney", "Entertainment Quarter, Moore Park", "20.10.2019", "19.00", "135 AUD");
 var Concert_S = new Event("sydney_6.jpg", "THE ROYAL EDINBURGH MILITARY TATTOO", "2000 Sydney", "ANZ Stadium, Sydney Olympic Park", "22.10.2019", "18.00", "155 AUD");
 var array = [Church_V, Zoo_V, Thai_V, Sixta_V, Music_V, Gallery_V, Opera_S, Pool_S, Greek_S, Italian_S, Cirque_S, Concert_S];
